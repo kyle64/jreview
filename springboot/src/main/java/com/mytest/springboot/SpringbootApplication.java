7b5810b83ee5428e9ddcefc45ea59a4ff5fd4334
kyle64 <244233177@qq.com>
2020-06-28 16:22:56 +0800

    update spring boot version

--- springboot/src/main/java/com/mytest/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/mytest/springboot/SpringbootApplication.java
@@ -1,8 +1,10 @@
 package com.mytest.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -10,6 +12,9 @@ public class SpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
+
+        String version = SpringVersion.getVersion();
+        String version1 = SpringBootVersion.getVersion();
     }
 
 }
